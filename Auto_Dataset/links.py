89b20c29052eaed937dda1a0d4b4b5e2a1e994f0
GLaDOS23 <150522506+GLaDOS23@users.noreply.github.com>
2024-08-04 13:20:35 +0300

    +links

---
+++ Auto_Dataset/links.py
@@ -0,0 +1,35 @@
+import youtube_dl
+
+def get_channel_videos(channel_url):
+    ydl_opts = {
+        'quiet': True,
+        'extract_flat': True,
+        'skip_download': True,
+        'force_generic_extractor': True
+    }
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        try:
+            result = ydl.extract_info(channel_url, download=False)
+            if 'entries' in result:
+                video_links = [entry['url'] for entry in result['entries']]
+                return video_links
+            else:
+                print("No videos found in the channel.")
+                return []
+        except Exception as e:
+            print(f"Error: {e}")
+            return []
+
+
+channel_url = 'https://www.youtube.com/c/@Name/videos'
+video_links = get_channel_videos(channel_url)
+
+if video_links:
+    for link in video_links:
+        print(link)
+else:
+    print("No videos found or an error occurred.")
+
+
+
